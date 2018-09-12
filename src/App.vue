73db18f6993215c56335447cd8cd38c775df829c
caishijian2008 <442754043@qq.com>
2018-09-12 18:01:34 +0800

    添加首頁内容

--- src/App.vue
+++ src/App.vue
@@ -121,7 +121,7 @@
               <img src="https://i8.mifile.cn/v1/a1/6d64ef02-bb5c-da49-45cb-7d6861885b29!144x152.webp" alt="">
             </a>
           </div>
-          <div class="divider-line"></div>
+          <div class="divider-line thick"></div>
           <div class="auto-fill column-cell">
             <a href="#" class="items">
               <img src="./assets/images/2018091018403546132.jpg" alt="">
@@ -134,13 +134,13 @@
             </a>
             <div class="clearfix"></div>
           </div>
-          <div class="divider-line"></div>
+          <div class="divider-line thick"></div>
           <div class="auto-fill">
             <a href="#" class="items">
               <img src="./assets/images/2018091020100945116.jpg" alt="">
             </a>
           </div>
-          <div class="divider-line"></div>
+          <div class="divider-line thick"></div>
           <div class="list-two flex-box">
             <a href="#" class="item">
               <div class="img">
@@ -175,8 +175,312 @@
               </div>
             </a>
           </div>
-
-
+          <div class="list-two flex-box">
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091113452071845.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米8 SE</div>
+                <div class="brief">三星 AMOLED 全面屏 小屏旗舰</div>
+                <div class="price">
+                  ￥1699<span>起</span>
+                  <span class="price old">￥<s>1799</s></span>
+                </div>
+              </div>
+            </a>
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091114283791347.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米Max 2</div>
+                <div class="brief">6.44"大屏，5300mAh大电量</div>
+                <div class="price">
+                  ￥1199<!--<span>起</span>-->
+                  <span class="price old">￥<s>1699</s></span>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="list-two flex-box">
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091113452071845.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米8 SE</div>
+                <div class="brief">三星 AMOLED 全面屏 小屏旗舰</div>
+                <div class="price">
+                  ￥1699<span>起</span>
+                  <span class="price old">￥<s>1799</s></span>
+                </div>
+              </div>
+            </a>
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091114283791347.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米Max 2</div>
+                <div class="brief">6.44"大屏，5300mAh大电量</div>
+                <div class="price">
+                  ￥1199<!--<span>起</span>-->
+                  <span class="price old">￥<s>1699</s></span>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="list-two flex-box">
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091113452071845.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米8 SE</div>
+                <div class="brief">三星 AMOLED 全面屏 小屏旗舰</div>
+                <div class="price">
+                  ￥1699<span>起</span>
+                  <span class="price old">￥<s>1799</s></span>
+                </div>
+              </div>
+            </a>
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091114283791347.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米Max 2</div>
+                <div class="brief">6.44"大屏，5300mAh大电量</div>
+                <div class="price">
+                  ￥1199<!--<span>起</span>-->
+                  <span class="price old">￥<s>1699</s></span>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="divider-line thick"></div>
+          <div class="auto-fill">
+            <a href="#" class="items">
+              <img src="./assets/images/2018091020100945116.jpg" alt="">
+            </a>
+          </div>
+          <div class="divider-line thick"></div>
+          <div class="list-two flex-box">
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091113452071845.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米8 SE</div>
+                <div class="brief">三星 AMOLED 全面屏 小屏旗舰</div>
+                <div class="price">
+                  ￥1699<span>起</span>
+                  <span class="price old">￥<s>1799</s></span>
+                </div>
+              </div>
+            </a>
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091114283791347.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米Max 2</div>
+                <div class="brief">6.44"大屏，5300mAh大电量</div>
+                <div class="price">
+                  ￥1199<!--<span>起</span>-->
+                  <span class="price old">￥<s>1699</s></span>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="list-two flex-box">
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091113452071845.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米8 SE</div>
+                <div class="brief">三星 AMOLED 全面屏 小屏旗舰</div>
+                <div class="price">
+                  ￥1699<span>起</span>
+                  <span class="price old">￥<s>1799</s></span>
+                </div>
+              </div>
+            </a>
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091114283791347.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米Max 2</div>
+                <div class="brief">6.44"大屏，5300mAh大电量</div>
+                <div class="price">
+                  ￥1199<!--<span>起</span>-->
+                  <span class="price old">￥<s>1699</s></span>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="divider-line thin"></div>
+          <div class="list-title flex-box">
+            <a href="#" class="h">
+              <div class="ti">更多小米XX产品 > </div>
+            </a>
+          </div>
+          <div class="divider-line thick"></div>
+          <div class="list-two flex-box">
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091113452071845.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米8 SE</div>
+                <div class="brief">三星 AMOLED 全面屏 小屏旗舰</div>
+                <div class="price">
+                  ￥1699<span>起</span>
+                  <span class="price old">￥<s>1799</s></span>
+                </div>
+              </div>
+            </a>
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091114283791347.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米Max 2</div>
+                <div class="brief">6.44"大屏，5300mAh大电量</div>
+                <div class="price">
+                  ￥1199<!--<span>起</span>-->
+                  <span class="price old">￥<s>1699</s></span>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="list-two flex-box">
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091113452071845.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米8 SE</div>
+                <div class="brief">三星 AMOLED 全面屏 小屏旗舰</div>
+                <div class="price">
+                  ￥1699<span>起</span>
+                  <span class="price old">￥<s>1799</s></span>
+                </div>
+              </div>
+            </a>
+            <a href="#" class="item">
+              <div class="img">
+                <img src="./assets/images/2018091114283791347.jpg" alt="">
+                <div class="tag">
+                  <img src="./assets/images/2018091113544399549.jpg" alt="">
+                </div>
+              </div>
+              <div class="info">
+                <div class="name">小米Max 2</div>
+                <div class="brief">6.44"大屏，5300mAh大电量</div>
+                <div class="price">
+                  ￥1199<!--<span>起</span>-->
+                  <span class="price old">￥<s>1699</s></span>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="divider-line thick"></div>
+          <div class="list-one-type2">
+            <a href="#" class="flex-box item">
+              <div class="img">
+                <img src="./assets/images/2018091116040932349.jpg" alt="">
+              </div>
+              <div class="info">
+                <div class="name">米家 iHealth 体温计</div>
+                <div class="briefwrap">又准又快，全家都爱用的体温计dddddddd</div>
+                <div class="price mt32">￥129</div>
+              </div>
+            </a>
+          </div>
+          <div class="list-one-type3">
+            <a href="#" class="flex-box item">
+              <div class="info">
+                <div class="name">米家便携电动剃须刀</div>
+                <div class="briefwrap">超便携，男人必备的第二把剃须刀</div>
+                <div class="price mt32">￥179</div>
+              </div>
+              <div class="img">
+                <img src="./assets/images/2018091117023052510.jpg" alt="">
+              </div>
+            </a>
+          </div>
+          <div class="list-one-type2">
+            <a href="#" class="flex-box item">
+              <div class="img">
+                <img src="./assets/images/2018091116040932349.jpg" alt="">
+              </div>
+              <div class="info">
+                <div class="name">米家 iHealth 体温计</div>
+                <div class="briefwrap">又准又快，全家都爱用的体温计</div>
+                <div class="price mt32">￥129</div>
+              </div>
+            </a>
+          </div>
+          <div class="divider-line thick"></div>
+          <div class="auto-fill">
+            <a href="#" class="items">
+              <img src="./assets/images/2018091020100945116.jpg" alt="">
+            </a>
+          </div>
+          <div class="divider-line thick"></div>
+          <div class="list-title flex-box">
+            <a href="#" class="h">
+              <div class="ti">了解我们</div>
+            </a>
+          </div>
+          <div class="divider-line thick"></div>
+          <!-- <BackTop :height="100" :bottom="100">
+            <div class="top">返回顶端</div>
+          </BackTop> -->
+          <BackTop :bottom="80"></BackTop>
         </div>
       </div>
 
@@ -460,10 +764,16 @@ ul {
       box-sizing: border-box;
       width: 100%;
       overflow: hidden;
-      height: 0.46rem;
-      // height: 0.16rem;
-      border-bottom: 0.16rem solid rgb(245, 245, 245);
-      background-color: rgb(245, 245, 245);
+      &.thick {
+        height: 0.46rem;
+        border-bottom: 0.46rem solid rgb(245, 245, 245);
+        background-color: rgb(245, 245, 245);
+      }
+      &.thin {
+        height: 0.1rem;
+        border-bottom: 0.1rem solid rgb(206, 206, 206);
+        background-color: rgb(206, 206, 206);
+      }
     }
     .auto-fill {
       position: relative;
@@ -525,15 +835,6 @@ ul {
           margin-right: .14rem;
         }
       }
-      .img {
-        position: relative;
-        // width: 3.6rem;
-        // height: 3.6rem;
-        img {
-          max-width: 100%;
-          height: auto;
-        }
-      }
       .tag {
         position: absolute;
         top: 0;
@@ -548,42 +849,133 @@ ul {
       .info {
         padding: .4rem .47rem;
         text-align: left;
-        .name, .brief {
-          white-space: nowrap;
-          overflow: hidden;
-          text-overflow: ellipsis;
-        }
-        .name {
-          font-size: .88rem;
-          color: rgba(0,0,0,.87);
-        }
-        .brief {
-          margin-top: .2rem;
-          font-size: .76rem;
-          line-height: .86rem;
-          color: rgba(0,0,0,.54);
-        }
-        .price {
-          font-size: .88rem;
-          color: #ea625b;
-          height: 1.5em;
-          line-height: 1.7em;
-          position: relative;
-          display: inline-block;
-          span {
-            display: inline-block;
-            margin-left: .14rem;
-            font-size: .62rem;
-          }
-        }
-        .price .old {
-          display: inline-block;
-          margin: 0 .1rem;
-          font-size: .62rem;
-          color: rgba(0,0,0,.54);
+      }
+    }
+    .img {
+      position: relative;
+      img {
+        max-width: 100%;
+        height: auto;
+      }
+    }
+    .name, .brief {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    .name {
+      font-size: .88rem;
+      color: rgba(0,0,0,.87);
+    }
+    .brief {
+      margin-top: .2rem;
+      font-size: .76rem;
+      line-height: 1rem;
+      color: rgba(0,0,0,.54);
+    }
+    .briefwrap {
+      margin-top: .2rem;
+      font-size: .76rem;
+      line-height: 1.3rem;
+      color: rgba(0,0,0,.54);
+    }
+    .price {
+      font-size: .88rem;
+      color: #ea625b;
+      height: 1.5em;
+      line-height: 1.7em;
+      position: relative;
+      display: inline-block;
+      span {
+        display: inline-block;
+        margin-left: .14rem;
+        font-size: .62rem;
+      }
+      &.mt32 {
+        margin-top: .2rem;
+      }
+    }
+    .price .old {
+      display: inline-block;
+      margin: 0 .1rem;
+      font-size: .62rem;
+      color: rgba(0,0,0,.54);
+    }
+    .list-title {
+      .h {
+        background: #fff;
+        height: 3rem;
+        line-height: 3rem;
+        font-size: .88rem;
+        text-align: center;
+        .ti {
+          color: rgba(0,0,0,.6);
         }
       }
     }
+    .list-one-type2 {
+      margin: 0;
+      padding: 0;
+      .item {
+        // width: 100%;
+        display: flex;
+        .img {
+          flex: 1;
+          img {
+            // width: 187px;
+            // height: 187px;
+            width: 100%;
+            height: auto;
+          }
+        }
+        .info {
+          flex:1;
+          padding: 2.64rem .32rem 0 0;
+          // padding: 2.64rem .22rem 0 .82rem;
+          box-sizing: border-box;
+          // width: 187px;
+          // height: 187px;
+          .name { padding-left: .88rem; }
+          .briefwrap { padding-left: .88rem; }
+          .price { padding-left: .88rem; }
+        }
+      }
+    }
+    .list-one-type3 {
+      margin: 0;
+      padding: 0;
+      .item {
+        // width: 100%;
+        display: flex;
+        .img {
+          flex: 1;
+          img {
+            // width: 188px;
+            // height: 188px;
+            width: 100%;
+            height: auto;
+          }
+        }
+        .info {
+          flex: 1;
+          padding: 2.64rem .22rem 0 0;
+          // padding: 2.64rem .22rem 0 .82rem;
+          box-sizing: border-box;
+          // width: 188px;
+          // height: 188px;
+          .name { padding-left: .88rem; }
+          .briefwrap { padding-left: .88rem; }
+          .price { padding-left: .88rem; }
+        }
+      }
+    }
+  }
+  .top{
+      padding: 10px;
+      background: rgba(0, 153, 229, .7);
+      color: #fff;
+      text-align: center;
+      border-radius: 2px;
   }
 }
 
